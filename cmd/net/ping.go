2a5414a995488533b24242fff5c31ac32708cf7a
Ruan Bekker <ruan.bekker@bvnk.com>
2023-04-18 22:10:09 +0200

    Add ping functionality

--- cmd/net/ping.go
+++ cmd/net/ping.go
@@ -6,26 +6,55 @@ package net
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var	(
 	urlPath string
+	client = http.Client {
+		Timeout: time.Second * 2,
+	}
 )
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+}
 
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
-	Short: "Ping is a subcommand of the net palette",
+	Short: "This pings a remote url and returns the response.",
 	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ping called")
+
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
+		}
 	},
 }
 
 func init() {
 	pingCmd.Flags().StringVarP(&urlPath, "url", "u", "", "the url to ping")
+
+	if err := pingCmd.MarkFlagRequired("url"); err != nil {
+		fmt.Println(err)
+	}
+
 	NetCmd.AddCommand(pingCmd)
 
 	// Here you will define your flags and configuration settings.
